8350687c6a5663ace7a8dbe2e137170c93ab1e60
29864892 <48174208+29864892@users.noreply.github.com>
2020-02-08 14:29:30 -0500

    city

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -13,8 +13,8 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'city', 'assets/city background.png' );
+        // Load an image and call it 'city'.
+        game.load.image( 'city', 'assets/city.png' );
     }
     
     var bckgrnd;
