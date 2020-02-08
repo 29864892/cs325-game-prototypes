4ed922b45ae9cb2c515e8b0019a376e8b02439ab
29864892 <48174208+29864892@users.noreply.github.com>
2020-02-08 14:20:13 -0500

    Update main.js

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -14,18 +14,17 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'city', 'assets/city background.png' );
     }
     
     var bouncy;
     
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
+        /* Create a sprite at the center of the screen using the 'logo' image.
         bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
-        
         // Turn on the arcade physics engine for this sprite.
         game.physics.enable( bouncy, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
@@ -35,8 +34,9 @@ window.onload = function() {
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
+        text.anchor.setTo( 0.5, 0.0 );*/
+		game.add.image(400,300, city);
+	}
     
     function update() {
         // Accelerate the 'logo' sprite towards the cursor,
@@ -44,6 +44,6 @@ window.onload = function() {
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+        //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
     }
 };
